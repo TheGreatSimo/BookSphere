907fd652f1c631e2fe1c7f9fa4b9b1127c12405d
Juba1806 <elaissaouimohammed01@gmail.com>
2024-01-15 10:38:22 +0100

    Added mongod

--- api/index.js
+++ api/index.js
@@ -1,7 +1,17 @@
+import mongoose from 'mongoose'
 import express from "express";
 
+mongoose.connect('mongodb://localhost/haja');
+
+const db = mongoose.connection;
 const app = express();
 
-app.listen(3000, () => {
-  console.log("the back is running on http://localhost:3000");
-});
+db.on('error', console.error.bind(console, 'MongoDb connectio error:'))
+
+db.once('open', () => {
+  console.log("Connection to Mongoode");
+  app.listen(3000, () => {
+    console.log("the back is running on http://localhost:3000");
+  });
+})
+
