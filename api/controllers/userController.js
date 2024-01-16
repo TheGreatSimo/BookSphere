5ba3bff4c66bcb4b6603292848043db4528c31ed
Juba1806 <elaissaouimohammed01@gmail.com>
2024-01-16 01:29:42 +0100

    Yeah bro the routing api works just fine

---
+++ api/controllers/userController.js
@@ -0,0 +1,5 @@
+export const test = (req,res) => {
+    res.json({
+        message: "it's working brother from  user controller"
+    })
+} 
--- api/index.js
+++ api/index.js
@@ -1,17 +1,28 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 import express from "express";
+import userRouter from './routes/user.route.js'
 
-mongoose.connect('mongodb://localhost/haja');
+mongoose.connect("mongodb://localhost/haja");
 
 const db = mongoose.connection;
 const app = express();
 
-db.on('error', console.error.bind(console, 'MongoDb connectio error:'))
+db.on("error", console.error.bind(console, "MongoDb connectio error:"));
 
-db.once('open', () => {
+db.once("open", () => {
   console.log("Connection to Mongoode");
-  app.listen(3000, () => {
-    console.log("the back is running on http://localhost:3000");
+});
+
+app.listen(3000, () => {
+  console.log("the back is running on http://localhost:3000");
+});
+
+
+app.use('/user', userRouter)
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "API is working",
   });
-})
+});
 
