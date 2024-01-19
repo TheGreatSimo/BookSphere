c620ba25f6daa61d7aaecd925e8758b2fc7cd6e1
Juba1806 <elaissaouimohammed01@gmail.com>
2024-01-19 07:55:08 +0100

    Add expiry date for token

--- api/controllers/authControllers.js
+++ api/controllers/authControllers.js
@@ -48,7 +48,9 @@ export const signin = async (req, res, next) => {
       return next(errorHandler(401, "Invalid password"));
     }
 
-    const token = jwt.sign({id: validUser._id }, process.env.JWT_SECRET)
+    const expiresIn = '1d';
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET, { expiresIn });
     const {password: hashPassword , ...rest} = validUser._doc
 
     res.cookie('usertoken', token, { httpOnly : true }).status(200).json(rest)
