1167d162730db8625c5fa665429f74bc10263ccd
Juba1806 <elaissaouimohammed01@gmail.com>
2024-01-21 11:50:55 +0100

    Making the profile picture && Updating the navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,10 +2,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React from "react";
 import Home from './pages/Home'
 import Profile from './pages/Profile'
+import About from "./pages/About";
 import Feed from './pages/Feed'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Navbar from './component/Navbar'
+import PrivateRoute from "./component/PrivateRoute";
 
 
 export default function App() {
@@ -14,10 +16,13 @@ export default function App() {
       <Navbar/>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route path="/feed" element={<Feed/>} />
+        <Route path="/about" element={<About />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/feed" element={<Feed/>} />
+        <Route element={<PrivateRoute />}>
+            <Route path="/profile" element={<Profile />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
--- client/src/component/Navbar.jsx
+++ client/src/component/Navbar.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 export default function NavBar() {
+  const { currentUser } = useSelector((state) => state.user)
   return (
     <nav className='bg-gray-600'>
       <div className='flex justify-between items-center max-w-7xl mx-auto p-5'>
@@ -14,17 +16,18 @@ export default function NavBar() {
             <li>Feed</li>
           </Link>
 
-          <Link to="/profile">
-            <li>Profile</li>
+          <Link to="/about">
+            <li>About</li>
           </Link>
 
-          <Link to="/sign-in">
-            <li>Sign-In</li>
+          <Link to='/profile'>
+            {currentUser ? (
+              <img src={currentUser.profilePicture} alt='profile' className='h-7 w-7 rounded-full object-cover' />
+            ) : (
+              <li>Sign In</li>
+            )}
           </Link>
 
-          <Link to="/sign-up">
-            <li>Sign-Up</li>
-          </Link>
           
         </ul>
 
