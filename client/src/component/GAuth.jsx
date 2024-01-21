218232b1058ca037d31e8bde66c786bab9c954e1
Juba1806 <elaissaouimohammed01@gmail.com>
2024-01-21 08:22:50 +0100

    Google auth working on the backend as well

--- client/src/component/GAuth.jsx
+++ client/src/component/GAuth.jsx
@@ -3,9 +3,11 @@ import { GoogleAuthProvider, signInWithPopup, getAuth } from 'firebase/auth';
 import { app } from '../firebase';
 import { useDispatch } from 'react-redux';
 import { signInSuccess } from '../redux/user/userSlice';
+import { useNavigate  } from "react-router-dom";
 
 export default function GAuth() {
   const dispatch = useDispatch(); // Move this line outside of handleGoogleAuth
+  const navigate = useNavigate();
 
   const handleGoogleAuth = async () => {
     try {
@@ -13,23 +15,32 @@ export default function GAuth() {
       const auth = getAuth(app);
 
       const result = await signInWithPopup(auth, provider);
-      const res = await fetch('api/auth/google', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          name: result.user.displayName,
-          email: result.user.email,
-          photo: result.user.photoURL,
-        }),
-      });
+      
 
-      const data = await res.json();
-      console.log(data);
+      try {
+          const res = await fetch('http://localhost:3000/api/auth/google', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
 
-      // Corrected: Call signInSuccess as a function and pass data if needed
-      dispatch(signInSuccess(data));
+            body: JSON.stringify({
+              "name": result.user.displayName,
+              "email": result.user.email,
+              "photo": result.user.photoURL
+            }),
+          });
+
+        
+        const data = await res.json();
+        dispatch(signInSuccess(data));
+        navigate('/')
+
+
+      } catch (error) {
+        console.log("Can't fetch data")
+        
+      }
     } catch (error) {
       console.log(error);
     }
